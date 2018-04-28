846cdda2f82ca57bd9fbbfa4eedf652614326be2
ttaeschn <torsten@taeschnix.de>
2018-04-28 18:45:37 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var handleReq = function(request, response) {
+  console.log('request for url: ' + request.url);
+  response.writeHead(200);
+  response.end('Hello World!');
+};
+var www = http.createServer(handleReq);
+www.listen(8080);
