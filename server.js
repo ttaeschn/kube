320b136e5a3eee036f3818d761f77403807b5a59
ttaeschn <torsten@taeschnix.de>
2018-04-28 21:02:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleReq = function(request, response) {
   console.log('request for url: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');
+  response.end('$DT Hello World!');
 };
 var www = http.createServer(handleReq);
 www.listen(8080);
